b0ed8f18e1cef08bd4c6cbfa57ced201375f25d2
helgisax <helgiingvarsson77@gmail.com>
2023-02-09 03:55:24 +0300

    fixed

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,4 +6,3 @@ const program = new Command();
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1', '-v, --vers', 'output the current version');
-  .option('-h, --help', 'display help for command');
