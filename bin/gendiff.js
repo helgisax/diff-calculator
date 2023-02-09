7d856a402fec08772782e502b3194e0a47cf9a04
helgisax <helgiingvarsson77@gmail.com>
2023-02-09 18:02:43 +0300

    fixed parse

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,3 +6,4 @@ const program = new Command();
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1', '-v, --vers', 'output the current version');
+program.parse();
