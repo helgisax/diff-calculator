4fcfa686bd9249ba2b99e3882e04ff30f4ac58e3
helgisax <helgiingvarsson77@gmail.com>
2023-02-09 03:49:59 +0300

    added description and version

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+const program = new Command();
+
+program
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1', '-v, --vers', 'output the current version');
+  .option('-h, --help', 'display help for command');
