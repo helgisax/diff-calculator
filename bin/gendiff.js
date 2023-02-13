b4ab5980a54c85cd4d9a7525ca9eb23c47a4230f
helgisax <helgiingvarsson77@gmail.com>
2023-02-13 05:22:25 +0300

    updated

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-
+import getDiff from '../src/getDiff.js';
 import { Command } from 'commander';
 const program = new Command();
 
@@ -8,5 +8,8 @@ program
   .version('0.0.1', '-v, --vers', 'output the current version')
   .argument('<filepath1>')
   .argument('<filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(getDiff(filepath1, filepath2));
+  })
   .option('-f, --format <type>', 'output format');
 program.parse();
