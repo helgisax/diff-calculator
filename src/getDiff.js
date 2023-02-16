bca8d78a8f791404ec6722ec5b733b6d96f5a80f
helgisax <helgiingvarsson77@gmail.com>
2023-02-16 05:16:22 +0300

    updated

--- src/getDiff.js
+++ src/getDiff.js
@@ -4,14 +4,12 @@ import { cwd } from 'process';
 import { resolve } from 'path';
 
 const getDiff = (filepath1, filepath2) => { 
-  const file1 = readFileSync(filepath1, 'utf-8');
-  const file2 = readFileSync(filepath2, 'utf-8');
-
-  console.log(JSON.parse(file1));
-  console.log(JSON.parse(file2));
+  const file1 = readFileSync(resolve(cwd(), (filepath1)), 'utf-8');
+  const file2 = readFileSync(resolve(cwd(), (filepath2)), 'utf-8');
 
   const obj1 = JSON.parse(file1);
   const obj2 = JSON.parse(file2);
+  console.log(obj1, obj2);
 };
 export default getDiff;
 
