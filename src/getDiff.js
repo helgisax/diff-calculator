6fafdd52ea77eacd212f93fc317d703f2d5fd8a9
helgisax <helgiingvarsson77@gmail.com>
2023-02-20 06:01:56 +0300

    updated, make new func

--- src/getDiff.js
+++ src/getDiff.js
@@ -9,7 +9,22 @@ const getDiff = (filepath1, filepath2) => {
 
   const obj1 = JSON.parse(file1);
   const obj2 = JSON.parse(file2);
-  console.log(obj1, obj2);
+
+  const keys = _.union(_.keys(obj1), _.keys(obj2));
+  
+  const difObj = {};
+    for (const key of keys) {
+      if (!_.has(obj1, key)) {
+        difObj[key] = 'added';
+      } else if (!_.has(obj2, key)) {
+        difObj[key] = 'deleted';
+      } else if (obj1[key] !== obj2[key]) {
+        difObj[key] = 'changed';
+      } else {
+        difObj[key] = 'unchanged';
+      }
+    }
+    return difObj;
 };
 export default getDiff;
 
