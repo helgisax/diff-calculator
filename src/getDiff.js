772a4f78372a029fa544dc334659ec628cf32d4b
helgisax <helgiingvarsson77@gmail.com>
2023-02-21 05:36:49 +0300

    updated

--- src/getDiff.js
+++ src/getDiff.js
@@ -10,21 +10,18 @@ const getDiff = (filepath1, filepath2) => {
   const obj1 = JSON.parse(file1);
   const obj2 = JSON.parse(file2);
 
-  const keysValues1 = Object.entries(obj1);
-  const keysValues2 = Object.entries(obj2);
-  const keysValues = _.union(keysValues1, keysValues2);
-  //const keys = _.union(_.keys(obj1), _.keys(obj2));
+  const keys = _.union(_.keys(obj1), _.keys(obj2));
   
   const difObj = {};
-    for (const [key, value] of keysValues) {
+    for (const key of keys) {
       if (!_.has(obj1, key)) {
-        difObj[key] = `+ ${value}`;
+        difObj[key] = `+, ${obj2[key]}`;
       } else if (!_.has(obj2, key)) {
-        difObj[key] = `- ${value}`;
+        difObj[key] = `-, ${obj1[key]}`;
       } else if (obj1[key] !== obj2[key]) {
-        difObj[key] = `changed? ${value}`;  // не очень понимаю как оформить логику, когда ключ есть в обоих объектах, но значения разные и нужно написать две строчки подряд, одну с +, а вторую с -.
+        difObj[key] = [obj1[key], obj2[key]];  // не очень понимаю как оформить логику, когда ключ есть в обоих объектах, но значения разные и нужно написать две строчки подряд, одну с +, а вторую с -.
       } else {
-        difObj[key] = `  ${value}`;
+        difObj[key] = ` , ${obj2[key]}`;
       }
     }
     return difObj;
