e30793354cca3cd6a106e9e2ad62a0099450d732
helgisax <helgiingvarsson77@gmail.com>
2023-02-21 03:52:47 +0300

    commit

--- src/getDiff.js
+++ src/getDiff.js
@@ -10,18 +10,21 @@ const getDiff = (filepath1, filepath2) => {
   const obj1 = JSON.parse(file1);
   const obj2 = JSON.parse(file2);
 
-  const keys = _.union(_.keys(obj1), _.keys(obj2));
+  const keysValues1 = Object.entries(obj1);
+  const keysValues2 = Object.entries(obj2);
+  const keysValues = _.union(keysValues1, keysValues2);
+  //const keys = _.union(_.keys(obj1), _.keys(obj2));
   
   const difObj = {};
-    for (const key of keys) {
+    for (const [key, value] of keysValues) {
       if (!_.has(obj1, key)) {
-        difObj[key] = '+';
+        difObj[key] = `+ ${value}`;
       } else if (!_.has(obj2, key)) {
-        difObj[key] = '-';
+        difObj[key] = `- ${value}`;
       } else if (obj1[key] !== obj2[key]) {
-        difObj[key] = 'changed?'; // не очень понимаю как оформить логику, когда ключ есть в обоих объектах, но значения разные и нужно написать две строчки подряд, одну с +, а вторую с -.
+        difObj[key] = `changed? ${value}`;  // не очень понимаю как оформить логику, когда ключ есть в обоих объектах, но значения разные и нужно написать две строчки подряд, одну с +, а вторую с -.
       } else {
-        difObj[key] = ' ';
+        difObj[key] = `  ${value}`;
       }
     }
     return difObj;
