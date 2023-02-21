6220c1f1a7d4ad22a2a0b248314ce1da97d974b9
helgisax <helgiingvarsson77@gmail.com>
2023-02-21 20:04:42 +0300

    made getDiff

--- src/getDiff.js
+++ src/getDiff.js
@@ -1,31 +1,29 @@
 import _ from 'lodash';
-import { readFileSync } from 'fs';
-import { cwd } from 'process';
-import { resolve } from 'path';
 
-const getDiff = (filepath1, filepath2) => { 
-  const file1 = readFileSync(resolve(cwd(), (filepath1)), 'utf-8');
-  const file2 = readFileSync(resolve(cwd(), (filepath2)), 'utf-8');
-
-  const obj1 = JSON.parse(file1);
-  const obj2 = JSON.parse(file2);
-
-  const keys = _.union(_.keys(obj1), _.keys(obj2));
+const getDiff = (obj1, obj2) => {
+  const keys = _.sortBy(_.union(_.keys(obj1), _.keys(obj2)));
+  const mknode = (key, value, type, oldValue = null) => ({ 
+    key,
+    value,
+    type,
+    oldValue
+  });
   
-  const difObj = {};
-    for (const key of keys) {
-      if (!_.has(obj1, key)) {
-        difObj[key] = `+, ${obj2[key]}`;
-      } else if (!_.has(obj2, key)) {
-        difObj[key] = `-, ${obj1[key]}`;
-      } else if (obj1[key] !== obj2[key]) {
-        difObj[key] = [obj1[key], obj2[key]];  // не очень понимаю как оформить логику, когда ключ есть в обоих объектах, но значения разные и нужно написать две строчки подряд, одну с +, а вторую с -.
-      } else {
-        difObj[key] = ` , ${obj2[key]}`;
-      }
-    }
-    return difObj;
+  const nodes = keys.map((key) => {
+    const [value1, value2] = [obj1[key], obj2[key]];
+    if (!_.has(obj1, key)) {
+      return mknode(key, value2, 'added');
+    } if (!_.has(obj2, key)) {
+      return mknode(key, value1, 'removed');
+    } if (obj1[key] !== obj2[key]) {
+      return mknode(key, value2, 'updated', value1);
+    } 
+    
+    return mknode(key, value1, 'unchanged');
+  })
+  return nodes;
 };
+
 export default getDiff;
 
 
