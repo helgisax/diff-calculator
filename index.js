b9125628e524f6d9ee01a78b295f5726b51c838a
helgisax <helgiingvarsson77@gmail.com>
2023-02-23 04:55:40 +0300

    fixed

--- index.js
+++ index.js
@@ -4,26 +4,26 @@ import { resolve } from 'path';
 import getDiff from './src/getDiff.js';
 
 const buildTree = (nodes) => {
-    const tree = nodes.map((node) => {
-      const {key, value, oldValue} = node;
-      switch(node.type) {
-        case 'added': return `  + ${key}: ${value}`
-        case 'removed': return `  - ${key}: ${value}`
-        case 'unchanged': return `    ${key}: ${value}`
-        case 'updated': return `  - ${key}: ${oldValue}\n  + ${key}: ${value}`
-        default: console.log('oops');
+  const tree = nodes.map((node) => {
+    const { key, value, oldValue } = node;
+    switch (node.type) {
+      case 'added': return `  + ${key}: ${value}`;
+      case 'removed': return `  - ${key}: ${value}`;
+      case 'unchanged': return `    ${key}: ${value}`;
+      case 'updated': return `  - ${key}: ${oldValue}\n  + ${key}: ${value}`;
+      default: throw new Error(`Unknown value, mate!: '${node}'`);
     }
-    })
-    return `{\n${tree.join('\n')} \n}`;
+  });
+  return `{\n${tree.join('\n')} \n}`;
 };
 
 const genDiff = (filepath1, filepath2) => {
-    const file1 = readFileSync(resolve(cwd(), (filepath1)), 'utf-8');
-    const file2 = readFileSync(resolve(cwd(), (filepath2)), 'utf-8');
+  const file1 = readFileSync(resolve(cwd(), (filepath1)), 'utf-8');
+  const file2 = readFileSync(resolve(cwd(), (filepath2)), 'utf-8');
 
-    const obj1 = JSON.parse(file1);
-    const obj2 = JSON.parse(file2);
+  const obj1 = JSON.parse(file1);
+  const obj2 = JSON.parse(file2);
 
-    return buildTree(getDiff(obj1, obj2));
+  return buildTree(getDiff(obj1, obj2));
 };
 export default genDiff;
