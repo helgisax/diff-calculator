732e129fdc0e885c6ccf2dcea694da9885c47ef0
helgisax <helgiingvarsson77@gmail.com>
2023-03-21 15:50:16 +0100

    new

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,10 +5,35 @@ import genDiff from '../index.js';
 const getFixturePath = (filename) => path.resolve(process.cwd(), '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-const genDiffResult = readFile('genDiff.expect.txt');
-const file1 = getFixturePath('file1.json');
-const file2 = getFixturePath('file2.json');
+const stylishResult = readFile('stylish.expect.txt');
+const plainResult = readFile('plain.expect.txt');
+const jsonResult = readFile('json.expect.txt');
 
-test('genDiff json', () => {
-  expect(genDiff(file1, file2)).toBe(genDiffResult);
+const file1Json = getFixturePath('file1.json');
+const file2Json = getFixturePath('file2.json');
+const file1Yml = getFixturePath('file1.yml');
+const file2Yml = getFixturePath('file2.yml');
+
+test.each([
+  [file1Json, file2Json, stylishResult],
+  [file1Yml, file2Yml, stylishResult],
+  [file1Json, file2Yml, stylishResult],
+])('Stylish', (file1, file2, expected) => {
+  expect(genDiff(file1, file2, 'stylish')).toBe(expected);
+});
+
+test.each([
+  [file1Json, file2Json, plainResult],
+  [file1Yml, file2Yml, plainResult],
+  [file1Json, file2Yml, plainResult],
+])('Plain', (file1, file2, expected) => {
+  expect(genDiff(file1, file2, 'plain')).toBe(expected);
+});
+
+test.each([
+  [file1Json, file2Json, jsonResult],
+  [file1Yml, file2Yml, jsonResult],
+  [file1Json, file2Yml, jsonResult],
+])('Plain', (file1, file2, expected) => {
+  expect(genDiff(file1, file2, 'json')).toBe(expected);
 });
