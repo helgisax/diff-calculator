90d01b1f55e3f4dfbaa7abec16489a9427258959
helgisax <helgiingvarsson77@gmail.com>
2023-03-21 15:09:51 +0100

    made formatter

---
+++ src/formatters/plain.js
@@ -0,0 +1,33 @@
+import _ from 'lodash';
+
+const makeString = (value) => {
+  if (_.isObject(value)) {
+    return '[complex value]';
+  }
+  return typeof value === 'string' ? `'${value}'` : value;
+};
+
+const plain = (nodes) => {
+  const iter = (node, parent = '') => {
+    const {
+      type, key, value, oldValue, children,
+    } = node;
+    switch (type) {
+      case 'nested':
+        return children.flatMap((child) => iter(child, `${parent}${key}.`)).join('\n');
+      case 'removed':
+        return `Property '${parent}${key}' was removed`;
+      case 'added':
+        return `Property '${parent}${key}' was added with value: ${makeString(value)}`;
+      case 'updated':
+        return `Property '${parent}${key}' was updated. From ${makeString(oldValue)} to ${makeString(value)}`;
+      case 'unchanged':
+        return [];
+      default: throw new Error(`Unknown type: ${type}`);
+    }
+  };
+  const result = nodes.map((node) => iter(node));
+  return `${result.join('\n')}`;
+};
+
+export default plain;
