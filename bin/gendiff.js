2efd3679f55d469c7fa89b7586b6e214084ddfca
helgisax <helgiingvarsson77@gmail.com>
2023-03-21 15:06:57 +0100

    updated

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -9,8 +9,8 @@ program
   .version('0.0.1', '-v, --vers', 'output the current version')
   .argument('<filepath1>')
   .argument('<filepath2>')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .action((filepath1, filepath2) => {
-    console.log(genDiff(filepath1, filepath2));
-  })
-  .option('-f, --format <type>', 'output format');
+    console.log(genDiff(filepath1, filepath2, program.opts().format));
+  });
 program.parse();
