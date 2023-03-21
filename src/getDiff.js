5298705a2c80f5b896f1430ed0fbe18311892b2f
helgisax <helgiingvarsson77@gmail.com>
2023-03-21 15:10:34 +0100

    rebuild tree

--- src/getDiff.js
+++ src/getDiff.js
@@ -1,27 +1,30 @@
 import _ from 'lodash';
 
+const mknode = (key, value, type, oldValue = null) => ({
+  key,
+  value,
+  type,
+  oldValue,
+});
+
 const getDiff = (obj1, obj2) => {
   const keys = _.sortBy(_.union(_.keys(obj1), _.keys(obj2)));
-  const mknode = (key, value, type, oldValue = null) => ({
-    key,
-    value,
-    type,
-    oldValue,
-  });
-
   const nodes = keys.map((key) => {
     const [value1, value2] = [obj1[key], obj2[key]];
-    if (!_.has(obj1, key)) {
-      return mknode(key, value2, 'added');
-    } if (!_.has(obj2, key)) {
-      return mknode(key, value1, 'removed');
-    } if (obj1[key] !== obj2[key]) {
-      return mknode(key, value2, 'updated', value1);
+    if (_.isObject(value1) && _.isObject(value2)) {
+      return {
+        type: 'nested',
+        key,
+        children: getDiff(value1, value2),
+      };
     }
+
+    if (!_.has(obj1, key)) return mknode(key, value2, 'added');
+    if (!_.has(obj2, key)) return mknode(key, value1, 'removed');
+    if (obj1[key] !== obj2[key]) return mknode(key, value2, 'updated', value1);
 
     return mknode(key, value1, 'unchanged');
   });
   return nodes;
 };
-
 export default getDiff;
