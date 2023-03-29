c5b60f625df165f7a389e862ecb5d495e3b1e778
helgisax <helgiingvarsson77@gmail.com>
2023-03-29 02:00:40 +0200

    fixed

--- src/getDiff.js
+++ src/getDiff.js
@@ -10,6 +10,14 @@ const mknode = (key, value, type, oldValue = null) => ({
 const getDiff = (obj1, obj2) => {
   const keys = _.sortBy(_.union(_.keys(obj1), _.keys(obj2)));
   const nodes = keys.map((key) => {
+    if (!_.has(obj1, key)) {
+      return mknode(key, obj2[key], 'added');
+    }
+
+    if (!_.has(obj2, key)) {
+      return mknode(key, obj1[key], 'removed');
+    }
+
     const [value1, value2] = [obj1[key], obj2[key]];
     if (_.isObject(value1) && _.isObject(value2)) {
       return {
@@ -17,14 +25,6 @@ const getDiff = (obj1, obj2) => {
         key,
         children: getDiff(value1, value2),
       };
-    }
-
-    if (!_.has(obj1, key)) {
-      return mknode(key, value2, 'added');
-    }
-
-    if (!_.has(obj2, key)) {
-      return mknode(key, value1, 'removed');
     }
 
     if (obj1[key] !== obj2[key]) {
