31f5ab7b87401881aedb8d2eeb114869517b6a10
helgisax <helgiingvarsson77@gmail.com>
2023-04-19 16:53:23 +0200

    fixed tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,28 +5,40 @@ import genDiff from '../src/index.js';
 const getFixturePath = (filename) => path.resolve(process.cwd(), '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
+const stylishResult = readFile('stylish.expect.txt');
+const plainResult = readFile('plain.expect.txt');
+
 const file1Json = getFixturePath('file1.json');
 const file2Json = getFixturePath('file2.json');
 const file1Yml = getFixturePath('file1.yml');
 const file2Yml = getFixturePath('file2.yml');
 
 test.each([
-  [file1Json, file2Json],
-  [file1Yml, file2Yml],
-  [file1Json, file2Yml],
-])('should return correct result in various formats', (file1, file2) => {
-  const stylishResult = readFile('stylish.expect.txt');
-  const plainResult = readFile('plain.expect.txt');
-
+  [file1Json, file2Json, stylishResult],
+  [file1Yml, file2Yml, stylishResult],
+  [file1Json, file2Yml, stylishResult],
+])('Stylish', (file1, file2, expected) => {
   const result1 = genDiff(file1, file2, 'stylish');
-  expect(result1).toBe(stylishResult);
+  expect(result1).toBe(expected);
 
   const result2 = genDiff(file1, file2, 'plain');
-  expect(result2).toBe(plainResult);
+  expect(result2).not.toBe(expected);
 
-  const result3 = genDiff(file1, file2, 'json');
-  expect(() => JSON.parse(result3)).not.toThrow();
+  const data = genDiff(file1, file2, 'json');
+  expect(() => JSON.parse(data)).not.toThrow();
+});
 
-  const result4 = genDiff(file1, file2);
-  expect(result4).toBe(stylishResult);
+test.each([
+  [file1Json, file2Json, plainResult],
+  [file1Yml, file2Yml, plainResult],
+  [file1Json, file2Yml, plainResult],
+])('Plain', (file1, file2, expected) => {
+  const result1 = genDiff(file1, file2, 'plain');
+  expect(result1).toBe(expected);
+
+  const result2 = genDiff(file1, file2, 'stylish');
+  expect(result2).not.toBe(expected);
+
+  const data = genDiff(file1, file2, 'json');
+  expect(() => JSON.parse(data)).not.toThrow();
 });
