78873080f4681d3032f237f1a878fa767869480f
helgisax <helgiingvarsson77@gmail.com>
2023-04-19 16:56:08 +0200

    fixed

--- src/getDiff.js
+++ src/getDiff.js
@@ -1,11 +1,11 @@
 import _ from 'lodash';
 
-const mknode = (key, type, value, value2 = null) => {
-  if (type === 'updated') {
-    return { key, type, value: [value, value2] };
-  }
-  return { key, value, type };
-};
+const mknode = (key, type, value, updatedValue = null) => ({
+  key,
+  value,
+  type,
+  updatedValue,
+});
 
 const getDiff = (obj1, obj2) => {
   const keys = _.sortBy(_.union(_.keys(obj1), _.keys(obj2)));
@@ -27,14 +27,12 @@ const getDiff = (obj1, obj2) => {
       };
     }
 
-    if (value1 !== value2) {
+    if (obj1[key] !== obj2[key]) {
       return mknode(key, value1, 'updated', value2);
     }
 
     return mknode(key, value1, 'unchanged');
   });
-
   return nodes;
 };
-
 export default getDiff;
